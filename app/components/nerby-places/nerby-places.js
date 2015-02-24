fdf51ce5275a5f7339f9787218be4847fec21bf4
Rasmus Eneman <rasmus@eneman.eu>
2015-02-24 13:50:07 +0100

    feat: Search for nerby places

---
+++ app/components/nerby-places/nerby-places.js
@@ -0,0 +1,46 @@
+'use strict';
+
+import Gourmet from '../../scripts/services/gourmet.js';
+
+class NerbyPlaces {
+	constructor(gourmet, observeOnScope, $scope) {
+    this.gourmet = gourmet;
+    this.places = [];
+    this.search = '';
+
+    observeOnScope($scope, 'ctrl.search')
+      .debounce(250)
+      .subscribe(this.filter.bind(this));
+
+    navigator.geolocation.getCurrentPosition(position => {
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+      this.filter();
+    });
+  }
+
+  filter() {
+    return this.gourmet.placesNear(this.latitude,this.longitude, this.search)
+      .then(places => this.places = places);
+  }
+}
+
+export default angular.module('nerbyPlaces', ['ngMaterial', 'rx', Gourmet.name])
+	.directive('nerbyPlaces', function() {
+		return {
+			templateUrl: 'components/nerby-places/nerby-places.html',
+			restrict: 'E',
+			scope: {
+				// Specify attributes where parents can pass and receive data here
+				// Syntax name: 'FLAG'
+				// FLAGS:
+				// = Two way data binding
+				// @ One way incoming expression (like placeholder)
+				// & One way outgoing behaviour (like ng-click)
+        test: '@'
+			},
+			bindToController: true,
+			controller: NerbyPlaces ,
+			controllerAs: 'ctrl'
+		};
+	});
---
+++ app/scripts/services/gourmet.js
@@ -0,0 +1,34 @@
+'use strict';
+
+class Gourmet {
+	constructor($http) {
+    this.$http = $http;
+    this.url = 'http://localhost:3000/v1';
+  }
+
+  get(path) {
+    var req = {
+      method: 'GET',
+      url: `${this.url}${path}`,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Key: 'b78d6ee724ccc781c8db08d37a375013'
+      },
+    };
+
+    return this.$http(req)
+      .then(data => {
+        data = data.data;
+        if (data.status !== 'success') { throw data.message; }
+        return data.data;
+      });
+  }
+
+  placesNear(lat, long, query) {
+    return this.get(`/places?query=${query}&location=${lat},${long}`);
+  }
+}
+
+export default angular.module('gourmet', [])
+	.service('gourmet', Gourmet );
