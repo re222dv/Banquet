009a6da32ab383b85e3571b0ea7e6f2218e2e1e6
Rasmus Eneman <rasmus@eneman.eu>
2015-03-04 11:30:17 +0100

    fix: Add missing files

---
+++ app/components/star-rating/star-rating.js
@@ -0,0 +1,27 @@
+'use strict';
+
+import PaperIcon from '../paper-icon/paper-icon.js';
+
+class StarRating {
+	constructor() {}
+}
+
+export default angular.module('starRating', [PaperIcon.name])
+	.directive('starRating', function() {
+		return {
+			templateUrl: 'components/star-rating/star-rating.html',
+			restrict: 'E',
+			scope: {
+				// Specify attributes where parents can pass and receive data here
+				// Syntax name: 'FLAG'
+				// FLAGS:
+				// = Two way data binding
+				// @ One way incoming expression (like placeholder)
+				// & One way outgoing behaviour (like ng-click)
+        rating: '=',
+			},
+			bindToController: true,
+			controller: StarRating ,
+			controllerAs: 'ctrl'
+		};
+	});
